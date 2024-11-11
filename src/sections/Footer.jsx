7f6fe715f47b3ea80ac0eac0fa255b2a61845297
Talha <mdtalha2008@gmail.com>
2024-11-11 23:52:11 +0600

    link add

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,6 +3,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiLinktree } from "react-icons/si";
 import { FaMediumM } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
@@ -26,7 +27,7 @@ const Footer = () => {
         <SiLinktree alt="github" className="w-1/2 h-1/2 text-white"/>
         </div>
         <div className="social-icon">
-        <FaMediumM alt="github" className="w-1/2 h-1/2 text-white"/>
+        <Link to=""><FaMediumM alt="github" className="w-1/2 h-1/2 text-white"/></Link>
         </div>
       </div>
       <p className="text-white-500">© 2024 Talha Tarique. All rights reserved.</p>
