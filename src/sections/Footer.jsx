7f1b04b6eda37169518b0060385215984d337347
Talha <mdtalha2008@gmail.com>
2024-11-11 23:51:01 +0600

    react router dom install

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,6 +2,7 @@ import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiLinktree } from "react-icons/si";
+import { FaMediumM } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
@@ -23,6 +24,9 @@ const Footer = () => {
         </div>
         <div className="social-icon">
         <SiLinktree alt="github" className="w-1/2 h-1/2 text-white"/>
+        </div>
+        <div className="social-icon">
+        <FaMediumM alt="github" className="w-1/2 h-1/2 text-white"/>
         </div>
       </div>
       <p className="text-white-500">© 2024 Talha Tarique. All rights reserved.</p>
