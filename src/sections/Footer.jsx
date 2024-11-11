9d0cbc97294ab5e424520cb717c5303ef723ed8b
Talha <mdtalha2008@gmail.com>
2024-11-11 21:09:07 +0600

    footer icon add

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,7 @@
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiLinktree } from "react-icons/si";
 const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
@@ -9,13 +13,16 @@ const Footer = () => {
 
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <FaGithub alt="github" className="w-1/2 h-1/2 text-white"/>
         </div>
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+        <FaXTwitter alt="github" className="w-1/2 h-1/2 text-white"/>
         </div>
         <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+        <FaLinkedinIn alt="github" className="w-1/2 h-1/2 text-white"/>
+        </div>
+        <div className="social-icon">
+        <SiLinktree alt="github" className="w-1/2 h-1/2 text-white"/>
         </div>
       </div>
       <p className="text-white-500">© 2024 Talha Tarique. All rights reserved.</p>
