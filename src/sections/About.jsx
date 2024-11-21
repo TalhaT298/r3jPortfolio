c463b875f01fe6648e19860ca3555afb6930a0a4
Talha <mdtalha2008@gmail.com>
2024-11-22 01:42:53 +0600

    about.jsx update

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
             <div>
               <p className="grid-headtext">Hi, I’m Talha Tarique</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
+                With 2+ years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
                 and responsive websites.
               </p>
             </div>
@@ -63,8 +63,8 @@ const About = () => {
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <p className="grid-subtext">I&apos;m based in Dhaka, Bangladesh and open to remote work worldwide.</p>
+              <a  href="https://github.com/TalhaT298" target="_blank" rel="noopener noreferrer"><Button name="Contact Me" isBeam containerClass="w-full mt-10" /></a>
             </div>
           </div>
         </div>
@@ -92,7 +92,7 @@ const About = () => {
             />
 
             <div className="space-y-2">
-              <p className="grid-subtext text-center">Contact me</p>
+              <a  href="https://github.com/TalhaT298" target="_blank" rel="noopener noreferrer"><p className="grid-subtext text-center">Contact me</p></a>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
                 <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">mdtalha2008@gmail.com</p>
